17fd83a14d6521a4b378085e33f60af7c351602c
Amdhgz <ahmed.zaher1923@gmail.com>
2023-09-10 21:52:23 +0000

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 # include <stdio.h>
+/** 
+* main is entry point
+*
+* Return 0 always
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts ("\"Programming is like building a multilingual puzzle");
 return(0);
 }
