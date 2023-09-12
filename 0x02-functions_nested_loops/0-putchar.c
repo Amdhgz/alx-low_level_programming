c01fbe5d82f4aa5416ec4ac3847ff216094ba331
Amdhgz <ahmed.zaher1923@gmail.com>
2023-09-12 22:32:07 +0000

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,10 @@ include "main.h"
 *Return: Always 0 (success)
 */
 int main(void)
-	char str[]="_putchar";
+	char str[] = "_putchar";
 	int ch;
-for(ch=0;ch<8;ch++)
-	_putchar(str[ch]);
-_putchar('\n');
-return(0);
+for (ch = 0; ch < 8; ch++)
+	_putchar (str[ch]);
+_putchar ('\n');
+return (0);
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 # ifndef MAIN_H
 # define MAIN_H
 # include <stdio.h>
-int __putchar(char c);
+int _putchar(char c);
 #endif
