d0972704798163778f266d413d191bc55fe58d50
Amdhgz <ahmed.zaher1923@gmail.com>
2023-09-13 13:57:15 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  *print_alphabet - utilize on _putchar to print the alphabet
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int ch;
 	for (ch = 'a'; ch<= 'z'; ch++)
-		_putchar(ch);
+		_putchar(ch);0
 	_putchar('\n');
 }
 			
